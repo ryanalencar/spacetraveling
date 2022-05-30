632eee009362b57243a60d0dcf3f2c4e2369c980
Ryan Alencar <ryanalencarbarbosa1701@gmail.com>
2022-05-30 20:01:18 -0300

    feat: posts data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import { FiCalendar, FiUser } from 'react-icons/fi';
+import { format, parseISO } from 'date-fns';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -25,10 +26,12 @@ interface PostPagination {
 }
 
 interface HomeProps {
-  postsPagination: PostPagination;
+  posts: PostPagination;
 }
 
-export default function Home({ postsPagination }: HomeProps) {
+export default function Home({ posts }: HomeProps) {
+  console.log(posts);
+
   return (
     <>
       <Head>
@@ -38,33 +41,38 @@ export default function Home({ postsPagination }: HomeProps) {
         <div className={styles.postsContainer}>
           <Image
             src="/imgs/logo.png"
-            alt="spacetraveling logo"
+            alt="logo"
             width="236"
             height="25"
             priority
           />
 
           <div className={styles.postsInfo}>
-            <Link href="/" passHref>
-              <a>
-                <strong className={styles.postTitle}>
-                  Como utilizar Hooks
-                </strong>
-                <p className={styles.postSubtitle}>
-                  Pensando em sincronização em vez de ciclos de vida
-                </p>
-                <div className={styles.postFooterInfo}>
-                  <div>
-                    <FiCalendar />
-                    <time>15 Mar 2022</time>
+            {posts.results.map(post => (
+              <Link href={`/post/${post.uid}`} passHref>
+                <a>
+                  <strong className={styles.postTitle}>
+                    {post.data.title}
+                  </strong>
+                  <p className={styles.postSubtitle}>{post.data.subtitle}</p>
+                  <div className={styles.postFooterInfo}>
+                    <div>
+                      <FiCalendar />
+                      <time>
+                        {format(
+                          parseISO(post.first_publication_date),
+                          'dd LLL yyyy'
+                        )}
+                      </time>
+                    </div>
+                    <div>
+                      <FiUser />
+                      <address>{post.data.author}</address>
+                    </div>
                   </div>
-                  <div>
-                    <FiUser />
-                    <address>Ryan Alencar</address>
-                  </div>
-                </div>
-              </a>
-            </Link>
+                </a>
+              </Link>
+            ))}
           </div>
         </div>
       </main>
@@ -75,6 +83,17 @@ export default function Home({ postsPagination }: HomeProps) {
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient({});
   const posts = await prismic.getByType('posts');
+
+  // const postsFormatted = posts.results.map(post => ({
+  //   slug: post.uid ?? '',
+  //   title: post.data.title,
+  //   subtitle: post.data.subtitle,
+  //   author: post.data.author,
+  //   updatedAt: format(parseISO(post.last_publication_date), 'dd LLL yyyy'),
+  // }));
+
+  // posts.results = postsFormatted;
+
   console.log(posts);
   return {
     props: {
